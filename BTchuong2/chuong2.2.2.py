927859b2971a93537ba595681509f4b81a1ab908
Actisle17 <lethihoamy17@gmail.com>
2023-03-10 14:47:00 +0700

    hi

--- BTchuong2/chuong2.2.2.py
+++ BTchuong2/chuong2.2.2.py
@@ -1,4 +1,5 @@
 r=int(input("Nhap vao ban kinh cua duong tron: "))
 Pi=float("3.14")
-print("Dien tich cua duong tron co ban kinh", r, "la",Pi*r**2)
-print("Chu vi cua duong tron co ban kinh", r, "la",2*r*Pi)
+    round
+print("Dien tich cua duong tron co ban kinh",r,"la",Pi*r**2)
+print("Chu vi cua duong tron co ban kinh",r,"la",2*r*Pi)
